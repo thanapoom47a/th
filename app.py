624f4c7d85c16de6523783dad59f75d97b9e0ff4
thanapoom47a <kingpoom16@gmail.com>
2025-07-17 09:28:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 app = Flask(__name__)
 bangkok_tz = pytz.timezone('Asia/Bangkok')
-MODEL_NAME = "gemini-1.5-flash-latest"
+MODEL_NAME = "gemini-2.5-flash-latest"
 emotions = ["😊", "😄", "🤔", "👍", "🙌", "😉", "✨"]
 
 try:
